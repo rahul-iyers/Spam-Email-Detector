9082dd5e998cce23402f0d634ecd60dfc3e2734b
Rahul Iyer <rahul1221.iyer@gmail.com>
2025-02-02 15:00:30 -0500

    updated for render

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from main import spamDetection
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
-#CORS(app)
-CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
+# CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -16,5 +21,6 @@ def predict():
     prediction = spamDetection(email)
     return jsonify({'result':prediction})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
